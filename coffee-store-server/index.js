209ef907925ea5657992d928b91c1a6f16d00c63
Sudip Kumar <keya24gadu2020@gmail.com>
2024-11-30 03:14:50 +0600

    created user

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -8,17 +8,9 @@ const port=process.env.PORT || 5000;
 //middleware
 app.use(cors())
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send('coffee shop server is running')
-})
-
-app.listen(port,()=>{
-    console.log(`server is running on port:${port}`)
-})
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nj8v5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,12 +25,41 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const coffeeCollection=client.db('coffeeDB').collection('coffee')
+
+    app.post('/coffee',async(req,res)=>{
+      const newCoffee=req.body;
+      console.log(newCoffee)
+      const result=await coffeeCollection.insertOne(newCoffee)
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send(result)
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('coffee shop server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port:${port}`)
+})
+
+
+
+
+
+
+
+
