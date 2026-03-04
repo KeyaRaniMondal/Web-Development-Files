fc65efabe7944df657ab63cc21ecd3f1bee8a4fd
KeyaRaniMondal <keyamondal549@gmail.com>
2026-03-05 02:46:06 +0600

    created basic setup for deployment

--- React/Ticket-System/src/Components/TicketCard.jsx
+++ React/Ticket-System/src/Components/TicketCard.jsx
@@ -1,68 +1,58 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { GoDotFill } from 'react-icons/go';
 
 const TicketCard = () => {
-const [tickets,setTickets]=useState([])
-    useEffect(()=>{
-const fetchTickets=async()=>{
-    const res=await fetch('/tickets.json')
-    const data=await res.json()
-    setTickets(data)
+    const [tickets, setTickets] = useState([])
+    useEffect(() => {
+        const fetchTickets = async () => {
+            const res = await fetch('/tickets.json')
+            const data = await res.json()
+            setTickets(data)
 
-};
-fetchTickets()
-    },[])
-console.log(tickets)
+        };
+        fetchTickets()
+    }, [])
+    console.log(tickets)
     // fetch('../../public/tickets.json')
     // .then(res=>res.json())
     // .then(data=>console.log(data))
-
-
-    createdAt
-: 
-// "2026-03-01T09:15:00Z"
-// customer
-// : 
-// "Acme Corp"
-// description
-// : 
-// "User unable to log in despite correct credentials. Error 401 returned."
-// id
-// : 
-// "TCK-001"
-// priority
-// : 
-// "High"
-// status
-// : 
-// "Open"
-// title
-// : 
-// "Logi
 
     return (
         <div className='flex justify-between'>
             <div>
                 <h1>Customer Tickets</h1>
-                <div>
+                <div className='grid grid-cols-2 gap-5'>
                     {
-                        tickets.map(tick=>(
+                        tickets.map(tick => (
                             <div className="card bg-base-100 w-96 shadow-sm">
-  <div className="card-body">
-    <h2 className="card-title">{tick.title}</h2>
-    <button className='btn-rounded-full'>{tick.status}</button>
-    <p>{tick.description}</p>
-<div className='flex justify-around'>
-    <div className='flex gap-3'>
-       <p>{tick.id}</p>
-       <p>{tick.priority}</p> 
-    </div>
-    <div className='flex gap-3'>
-        <p>{tick.customer}</p>
-        <p>{tick.createdAt}</p>
-    </div>
-</div>
-  </div>
-</div>
+                                <div className="card-body">
+                                    <span className='flex justify-between'>
+                                        <h2 className="card-title">{tick.title}</h2>
+                                        <button className={`btn btn-sm rounded-full ${
+                                                (() => {
+                                                    const s = tick.status.toLowerCase();
+                                                    if (s === 'open') return 'btn-success';
+                                                    if (s === 'in progress' || s === 'in-progress') return 'btn-warning';
+                                                    return '';
+                                                })()
+                                            }`}>
+                                            <GoDotFill />{tick.status}
+                                        </button>
+                                    </span>
+
+                                    <p>{tick.description}</p>
+                                    <div className='flex justify-around'>
+                                        <div className='flex gap-3'>
+                                            <p>{tick.id}</p>
+                                            <p>{tick.priority}</p>
+                                        </div>
+                                        <div className='flex gap-3'>
+                                            <p>{tick.customer}</p>
+                                            <p>{tick.createdAt}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         ))
                     }
                 </div>
