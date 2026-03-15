cae50d3b79e9590867e3d0142a0a3805dc8f89bc
KeyaRaniMondal <keyamondal549@gmail.com>
2026-03-16 03:25:50 +0600

    seted up router and layout

--- AppCreato/src/main.jsx
+++ AppCreato/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import MainLayout from './Layouts/MainLayout.jsx'
+import Home from './Home.jsx'
 
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children:[
+      {
+        index: true,
+        element:<Home/>
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
