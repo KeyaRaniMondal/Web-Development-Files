1fba3a48f0d02ea88f5980ab3a6e10fa163a03d3
KeyaRaniMondal <keyamondal549@gmail.com>
2026-03-16 21:26:42 +0600

    showed number of reviews, downloads and active apps

--- AppCreato/src/Home.jsx
+++ AppCreato/src/Home.jsx
@@ -5,25 +5,29 @@ import { useLoaderData } from 'react-router';
 
 const Home = () => {
   const apps=useLoaderData();
+  const totalDownloads = apps.reduce((sum, app) => sum + app.downloads, 0);
+  const totalReviews = apps.reduce((sum, app) => sum + app.reviews, 0);
+  const activeApps = apps.length;
   return (
     <div>
       <Banner />
-      <div>
-        <h1>Trusted by Millions, Built for You</h1>
-        <div>
-          <div>
+      {/* calculation of reviews, downloads and active apps */}
+      <div className='w-full h-[310px] bg-gradient-to-r from-purple-600 to-purple-400 text-white flex flex-col items-center justify-center gap-5'>
+        <h1 className='text-5xl font-bold'>Trusted by Millions, Built for You</h1>
+        <div className='flex justify-around gap-20'>
+          <div >
             <p>Total Downloads</p>
-            <h1></h1>
+            <h1 className='text-4xl font-bold'>{totalDownloads}</h1>
             <p>21% more than last month</p>
           </div>
           <div>
             <p>Total Reviews</p>
-            <h1></h1>
+            <h1 className='text-4xl font-bold'>{totalReviews}</h1>
             <p>46% more than last month</p>
           </div>
           <div>
             <p>Active Apps</p>
-            <h1></h1>
+            <h1 className='text-4xl font-bold'>{activeApps}</h1>
             <p>31 more will Launch</p>
           </div>
         </div>
